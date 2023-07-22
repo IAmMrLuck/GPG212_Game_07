f624dc70fe376b2fdbc41012940ce90ac0ec0444
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-07-22 19:51:01 +1000

    made the 4 rooms, and the transition for each camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform target;
+    [SerializeField] private Transform targetPlayer;
     [SerializeField] private float smoothing;
 
-    [SerializeField] private Vector2 minPos;
-    [SerializeField] private Vector2 maxPos;
+    public Vector2 minPos;
+    public Vector2 maxPos;
 
 
     private void FixedUpdate()
     {
-        if(transform.position != target.position)
+        if (transform.position != targetPlayer.position)
         {
-            Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+            Vector3 targetPos = new Vector3(targetPlayer.position.x, targetPlayer.position.y, transform.position.z);
 
             targetPos.x = Mathf.Clamp(targetPos.x, minPos.x, maxPos.x);
             targetPos.y = Mathf.Clamp(targetPos.y, minPos.y, maxPos.y);
--- Assets/Scripts/RoomTrigger.cs
+++ Assets/Scripts/RoomTrigger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class RoomTrigger : MonoBehaviour
@@ -9,5 +10,27 @@ public class RoomTrigger : MonoBehaviour
 
     [SerializeField] private Vector3 playerChange;
 
-    
+    [SerializeField] CameraFollow camFollow;
+
+    private void Start()
+    {
+        camFollow = Camera.main.GetComponent<CameraFollow>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            TransitionRoom(collision);
+        }
+
+    }
+
+    private void TransitionRoom(Collider2D collision)
+    {
+        camFollow.minPos += camMinChange;
+        camFollow.maxPos += camMaxChange;
+
+        collision.transform.position += playerChange;
+    }
 } 
